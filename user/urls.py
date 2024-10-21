9b5ab7aa45ab98f96abd7f880ec20404733659ed
Vikas Maurya <mauryavikas5310@gmail.com>
2024-10-21 20:08:55 +0530

    news
    inital commit

---
+++ user/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('index/', views.index),
+    path('', views.index),
+    path('about/', views.about),
+    path('contact/', views.contact),
+    path('jobs/', views.myjobs),
+    path('news/', views.news),
+    path('videos/', views.videos),
+    path('viewnews/', views.viewnews),
+    path('privacy/', views.privacy),
+    path('vikas/', views.vikas),
+]
